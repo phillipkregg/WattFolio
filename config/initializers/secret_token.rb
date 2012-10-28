5c978f79c8d2bdc5a4b991d11e429a5b16aff333
Your Name <phillipkregg@gmail.com>
2012-10-27 23:39:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wattfolio::Application.config.secret_token = '6cdb929c67d9790c04007f819c622f557e4530327995b966f08f8b832fedbbb05a39b0b41e607a657c96806a06c2709a7f874776f4466ad136e2baf846d3702d'
